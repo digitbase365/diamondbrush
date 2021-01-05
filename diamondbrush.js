5219fdbceb4ffdb8685e8c3c1ac689b42d4180cc
unknown <darlsele@gmail.com>
2021-01-05 12:27:40 +0100

    Initial Upload...

---
+++ diamondbrush.js
@@ -0,0 +1,17 @@
+
+// Slider Script...
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 33,
+    stretch: 0,
+    depth: 0,
+    modifier: 1,
+    slideShadows: true,
+  },
+ loop: true,
+});
+// End of Slider Script...
